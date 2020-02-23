4f604cef30fe8d55c9ece765f0824282ed3f2c35
daisy <sundaisy1007@outlook.com>
2020-02-23 01:24:34 +0000

    finish flow

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+
+
+if(process.env.NODE_ENV === 'production'){
+	
+	// we are in production, return production key
+	
+	module.exports = require('./prod');
+	
+	
+} else{
+
+	// we are in development, return dev key
+	
+	module.exports = require('./dev');
+	
+	
+	
+}
